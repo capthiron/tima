062f17010ad1867858d52239ef86b36fd8563e68
capthiron <24925821+capthiron@users.noreply.github.com>
2020-08-29 14:06:15 +0200

    Add task.DefaultRepository and move model.Task -> task.Task

--- task/repository.go
+++ task/repository.go
@@ -1,12 +1,27 @@
 package task
 
-import (
-	"github.com/capthiron/tima/model"
-)
-
 type Repository interface {
-	AddTask(task *model.Task) (*model.Task, error)
-	GetTasks() ([]model.Task, error)
-	RemoveTask(id string) error
+	AddTask(task *Task) (*Task, error)
+	GetTasks() ([]Task, error)
+	UpdateTask(task *Task) (*Task, error)
+	RemoveTask(id uint) error
 }
 
+type DefaultRepository struct {
+}
+
+func (r DefaultRepository) AddTask(task *Task) (*Task, error) {
+	return nil, nil
+}
+
+func (r DefaultRepository) GetTasks() ([]Task, error) {
+	return nil, nil
+}
+
+func (r DefaultRepository) UpdateTask(task *Task) (*Task, error) {
+	return nil, nil
+}
+
+func (r DefaultRepository) RemoveTask(id uint) error {
+	return nil
+}
