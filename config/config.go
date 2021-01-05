28092cf0359eebd491dfe802318475e013a50f50
dleitter <dario.leittersdorf@bonprix.net>
2021-01-06 00:00:23 +0100

    Implement and test the basic config package

--- config/config.go
+++ config/config.go
@@ -1,1 +1,12 @@
 package config
+
+import "gorm.io/gorm"
+
+type Config struct {
+	gorm.Model
+	DayLength            float64
+	DefaultBreakDuration int
+	DateFormat           DateFormat `gorm:"default"`
+}
+
+type DateFormat string
